01953cc15f56536a8ad211f866f29507ecf5030c
Sam Richards <samrrichards@gmail.com>
2017-02-15 19:30:53 -0800

    completed exercise 6

--- part6/Main.elm
+++ part6/Main.elm
@@ -26,9 +26,9 @@ searchResultDecoder =
     --
     -- TODO replace these calls to `hardcoded` with calls to `required`
     decode SearchResult
-        |> hardcoded 0
-        |> hardcoded ""
-        |> hardcoded 0
+        |> required "id" int
+        |> required "name" string
+        |> required "stargazers_count" int
 
 
 type alias Model =
@@ -60,19 +60,10 @@ responseDecoder =
 decodeResults : String -> List SearchResult
 decodeResults json =
     case decodeString responseDecoder json of
-        -- TODO add branches to this case-expression which return:
-        --
-        -- * the search results, if decoding succeeded
-        -- * an empty list if decoding failed
-        --
-        -- see http://package.elm-lang.org/packages/elm-lang/core/4.0.0/Json-Decode#decodeString
-        --
-        -- HINT: decodeString returns a Result which is one of the following:
-        --
-        -- Ok (List SearchResult)
-        -- Err String
-        _ ->
-            []
+      Ok results ->
+        results
+      _ ->
+          []
 
 
 view : Model -> Html Msg
@@ -98,7 +89,6 @@ viewSearchResult result =
         , button [ class "hide-result", onClick (DeleteById result.id) ]
             [ text "X" ]
         ]
-
 
 type Msg
     = SetQuery String
